7418c37aa192eb2c968f41c754e17e3e7a361f97
Ana María <anicu2314@gmail.com>
2024-08-26 10:26:33 -0500

    Acomodación de carpetas

--- src/main/java/co/edu/uniquindio/unieventos/Main.java
+++ src/main/java/co/edu/uniquindio/unieventos/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package co.edu.uniquindio.unieventos;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
