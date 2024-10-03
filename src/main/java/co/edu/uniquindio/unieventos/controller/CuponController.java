8cff34a5677f3acf44f61d3304cdb6126a1751a7
Miguel Florez <florezvasquezjuanmiguel@gmail.com>
2024-10-03 00:25:53 -0500

    Correcciones para post y front

--- src/main/java/co/edu/uniquindio/unieventos/controller/CuponController.java
+++ src/main/java/co/edu/uniquindio/unieventos/controller/CuponController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.http.HttpResponse;
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/cupon")
 public class CuponController {
